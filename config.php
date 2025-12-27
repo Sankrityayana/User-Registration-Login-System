b0c7ddc09b5d9ef7b8a32ab32b0194598eec6ecd
Sankrityayana <shanbhagsuraj84@gmail.com>
2025-12-27 18:33:25 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,9 +4,10 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'user_system');
+define('DB_PORT', 3307);
 
 // Create connection
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
 // Check connection
 if ($conn->connect_error) {
